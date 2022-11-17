a577fdfd24372ac4e67faf96e443d50cd499c080
pedrodecampos <pedroafonso939@yahoo.com.br>
2022-11-17 17:28:58 -0300

    Att

---
+++ projeto/src/main/java/br/com/criandoapi/projeto/repository/IUsuario.java
@@ -0,0 +1,12 @@
+package br.com.criandoapi.projeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import br.com.criandoapi.projeto.model.Usuario;
+
+public interface IUsuario extends JpaRepository<Usuario, Integer>{
+	public Usuario findByEmail(String email);
+	
+
+}
