a3dc45526265f992c196900c2578e671843f59bd
BeatrizOs1 <76188108+BeatrizOs1@users.noreply.github.com>
2022-12-15 15:25:31 -0300

    Add files via upload

---
+++ src/Front-End/Passagens/passagens.js
@@ -0,0 +1,99 @@
+let passagemJson = [
+    {
+        id: 1,
+        name: 'Fernando de Noronha',
+        img: 'images/noronha.png',
+        price: [440.00, 690.00, 990.00],
+        sizes: [
+          '1 pessoa',
+          '2 pessoas',
+          '1 pessoa'
+        ],
+        description: 'Localizado em Pernambuco'
+      },
+      {
+        id: 2,
+        name: 'Curitiba',
+        img: 'images/curitiba.png',
+        price: [350.00, 700.00, 950.00],
+        sizes: [
+          '1 pessoa',
+          '2 pessoas',
+          '1 pessoa'
+        ],
+        description: 'Localizado no Paraná'
+      },
+      {
+        id: 3,
+        name: 'Foz do Iguaçu',
+        img: 'images/foz.png',
+        price: [270.00, 410.00, 800.00],
+        sizes: [
+          '1 pessoa',
+          '2 pessoas',
+          '1 pessoa'
+        ],
+        description: 'Localizado no Paraná'
+      },
+      {
+        id: 4,
+        name: 'Capitólio',
+        img: 'images/capitolio.png',
+        price: [250.00, 450.00, 500.00],
+        sizes: [
+          '1 pessoa',
+          '2 pessoas',
+          '1 pessoa'
+        ],
+        description: 'Localizado em Minas Gerais'
+      },
+      {
+        id: 5,
+        name: 'Raul Soares',
+        img: 'images/raul.png',
+        price: [450.00, 850.00, 900.00],
+        sizes: [
+          '1 pessoa',
+          '2 pessoas',
+          '1 pessoa'
+        ],
+        description: 'Localizado em Minas Gerais'
+      },
+      {
+        id: 6,
+        name: 'São Paulo',
+        img: 'images/sp.png',
+        price: [310.00, 590.00, 620.00],
+        sizes: [
+          '1 pessoa',
+          '2 pessoas',
+          '1 pessoa'
+        ],
+        description: 'Localizado em São Paulo'
+      },
+      {
+        id: 7,
+        name: 'Rio de Janeiro',
+        img: 'images/rio.png',
+        price: [310.00, 590.00, 620.00],
+        sizes: [
+          '1 pessoa',
+          '2 pessoa',
+          '1 pessoa'
+        ],
+        description: 'Localizado no Rio de Janeiro'
+      },
+      {
+        id: 8,
+        name: 'Bonito',
+        img: 'images/bonito.png',
+        price: [500.00, 450.00, 999.00],
+        sizes: [
+          '1 pessoa',
+          '2 pessoas',
+          '1 pessoa'
+        ],
+        description: 'Localizado em Mato Grosso do Sul'
+      }
+      
+];
